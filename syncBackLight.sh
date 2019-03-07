19cd9c7df93bc774a09b4c6f32dfca9d694de8ba
huruixin <huruixin@ruijie.com.cn>
2019-03-07 17:51:07 +0800

    com third

--- syncBackLight.sh
+++ syncBackLight.sh
@@ -3,6 +3,9 @@ sysMaxBltPath="/sys/class/backlight/intel_backlight/max_brightness"
 sysBltPath="/sys/class/backlight/intel_backlight/brightness"
 sysActualBltPath="/sys/class/backlight/intel_backlight/actual_brightness"
 bltConfPath="/etc/UserCfg/backlightBrightness"
+bltConfDir="/etc/UserCfg"
+sysPciDriverUnbind="/sys/bus/pci/devices/0000\:00\:02.0/driver/unbind"
+sysPciDriverBind="/sys/bus/pci/drivers/i915/bind"
 
 function checkGpuDriverExist()
 {
@@ -26,46 +29,63 @@ function checkBackLightDriverExist()
 function loadGpuDriver()
 {
 	/sbin/modprobe i915
-	echo 0000:00:02.0 > /sys/bus/pci/devices/0000\:00\:02.0/driver/unbind
-	echo 0000:00:02.0 > /sys/bus/pci/drivers/i915/bind
+	if [ -f $sysPciDriverUnbind ]; then
+		echo 0000:00:02.0 > $sysPciDriverUnbind
+	fi
+	echo 0000:00:02.0 > $sysPciDriverBind
+}
+
+function setDefaultBrightness()
+{
+	local bltMax=`cat $sysMaxBltPath`
+	local bltDefault=$(expr $bltMax \* 8 / 10)
+	if [ ! -f $bltConfPath ]; then
+		echo "maxBrightness=$bltMax" > $bltConfPath
+		echo "actBrightness=$bltDefault" >> $bltConfPath
+	fi
+	echo $bltDefault > $sysBltPath
 }
 
 function restoreBltValue()
 {
 	if [ "$(checkGpuDriverExist)" = "true" ] && [ "$(checkBackLightDriverExist)" = "true" ]; then # check  the i915 and backlight driver
-		if [ ! -f $bltConfPath ]; then    # if the file no exist set the backlight value in 80% of the maximum
-			bltMax=`cat $sysMaxBltPath`
-			bltDefault=$(expr $bltMax \* 8 / 10)
-			echo "maxBrightness=$bltMax" > $bltConfPath
-			echo "brightness=$bltDefault" >> $bltConfPath
-			echo $bltDefault > $sysBltPath
+		if [ ! -f $bltConfPath ]; then    # if the file not exist set the backlight value in 80% of the maximum
+			if [ ! -f $bltConfDir ]; then  #if the config dir not exist , create it
+				mkdir $bltConfDir
+			fi
+			setDefaultBrightness
 		else
-				bltDefault=`cat $bltConfPath | grep '^brightness' | awk -F "=" '{print $2}'`  # set the backlight value which save in file
-				echo $bltDefault > $sysBltPath
+				bltDefault=`cat $bltConfPath | grep '^actBrightness' | awk -F "=" '{print $2}'`  # set the backlight value which save in file
+				if [ ! $bltDefault ]; then # if not get brightness, set the backlight value in 80% of the maximum
+					setDefaultBrightness
+				else
+					echo $bltDefault > $sysBltPath
+				fi
 		fi
 	fi
 }
 
+#param: $1(maxBrightness of save) $2(maxBrightness of sys) $3(brightness of save)
 function checkMaxBlt()
 {
 	if [ $1 -ne $2 ]; then #if the maxValue of backlight changed
 		sed -i "s/^maxBrightness=.*/maxBrightness=$2/" $bltConfPath 
-		sed -i "s/^brightness=.*/brightness=$(expr $2 \* $3 / $1)/" $bltConfPath #save the new brightness in old percent
+		sed -i "s/^actBrightness=.*/actBrightness=$(expr $2 \* $3 / $1)/" $bltConfPath #save the new brightness in old percent
 	fi
 }
 
+#param: $1(maxBrightness of sys)
 function checkUserBlt()
 {
 	local bltUser=`cat $sysActualBltPath`
-    local bltDefault=`cat $bltConfPath | grep '^brightness' | awk -F "=" '{print $2}'`
-	echo "bltUser=$bltUser bltDefault=$bltDefault"
+	local bltDefault=`cat $bltConfPath | grep '^actBrightness' | awk -F "=" '{print $2}'`
 	if [ $bltUser -eq 0 ]; then  # if the backlight value is zero , set the 10% of max_brightness and save it.
 		bltDefault=$(expr $1 / 10)
-		sed -i "s/^brightness=.*/brightness=$bltDefault/" $bltConfPath
+		sed -i "s/^actBrightness=.*/actBrightness=$bltDefault/" $bltConfPath
 		echo $bltDefault > $sysBltPath
 	else
 		if [ $bltUser -ne $bltDefault ]; then
-			sed -i "s/^brightness=.*/brightness=$bltUser/" $bltConfPath
+			sed -i "s/^actBrightness=.*/actBrightness=$bltUser/" $bltConfPath
 		fi
 	fi
 }
@@ -74,7 +94,7 @@ function checkAndSaveBlt()
 {	
 	if [ "$(checkBackLightDriverExist)" = "true" ]; then #check the backlight driver
 		local maxDefaultBltMax=`cat $bltConfPath | grep '^maxBrightness' | awk -F "=" '{print $2}'`
-		local bltDefault=`cat $bltConfPath | grep '^brightness' | awk -F "=" '{print $2}'`
+		local bltDefault=`cat $bltConfPath | grep '^actBrightness' | awk -F "=" '{print $2}'`
 		local sysBltMax=`cat $sysMaxBltPath`
 		checkMaxBlt $maxDefaultBltMax $sysBltMax $bltDefault # if maxBrightness change update the maxBrightness and brightness
 		checkUserBlt $sysBltMax
@@ -82,25 +102,15 @@ function checkAndSaveBlt()
 }
 
 function saveBltValue()
-{
-	if [ "$(checkGpuDriverExist)" = "true" ]; then  	# if i915 driver is exist.
-		checkAndSaveBlt
-	else
-		loadGpuDriver   # i915 driver not exist add the i915 driver for sync the backlight value.	
-		checkAndSaveBlt
-	fi
+{	
+	if [ "$(checkGpuDriverExist)" = "false" ]; then  	# if i915 driver is exist.
+		loadGpuDriver   # i915 driver not exist add the i915 driver for sync the backlight value.
+	fi	
+	checkAndSaveBlt
 }
 
 if [ $# -ne 0 ]; then
 	case "$1" in
-		poweroff)
-			saveBltValue
-			/bin/systemctl --force poweroff
-			;;
-		reboot)
-			saveBltValue
-			/bin/systemctl --force reboot
-			;;
 		restoreBlt)
 			restoreBltValue
 			;;
@@ -112,4 +122,5 @@ if [ $# -ne 0 ]; then
 			;;
 	esac
 fi
+
 
